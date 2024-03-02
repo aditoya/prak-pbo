ea61cc09aa6b7dae7edd20cc85435addfef1bea3
Aditya Wahyu Suhendar <115224136+aditoya@users.noreply.github.com>
2024-03-02 23:55:22 +0700

    Update 122140235_Aditya Wahyu Suhendar_Prak1_soal2.py

--- prak1/122140235_Aditya Wahyu Suhendar_Prak1_soal2.py
+++ prak1/122140235_Aditya Wahyu Suhendar_Prak1_soal2.py
@@ -7,19 +7,19 @@ Soal 2 (Mencari Luas & Keliling Lingkaran)
 
 from math import pi # mengimport pi untuk kebutuhan perhitungan lingkaran, phi = 3.14 atau 22/7
 
-def Hitung(jari):    
-    luas = pi * jari **2
-    keliling = 2 * pi * jari 
-    return luas, keliling
+def Hitung(jari): # fungsi bernama Hitung dengan parameter jari    
+    luas = pi * jari **2  # mencari luas lingkaran 
+    keliling = 2 * pi * jari  # mencari  keliling lingkaran
+    return luas, keliling # mengembalikan nilai luas dan keliling lingkaran
 
-try:
-    jari = float(input("jari-jari : "))
+try: # digunakan untuk menjalankan blok kode dan menangkap error yang mungkin terjadi.
+    jari = float(input("Inputkan jari-jari : ")) # user menginputkan jari-jari
     
-    if jari >= 0:
-        luas, keliling = Hitung(jari)
-        print(f"Luas: {luas:.2f}")
-        print(f"Keliling: {keliling:.2f}")
-    else:
+    if jari >= 0: # jika jari-jari yang dinputkan user lebih dari sama dengan 0, maka
+        luas, keliling = Hitung(jari) # jari-jari yang diinputkan user akan dihitung dengan memanggil fungsi  Hitung()
+        print(f"Luas: {luas:.2f}") # 2f berfungsi agar maksimal 2 angka dibelakang koma
+        print(f"Keliling: {keliling:.2f}") # 2f berfungsi agar maksimal 2 angka dibelakang koma
+    else: # jika tidak
         print("jari-jari lingkaran tidak boleh negatif")
 except ValueError: # akan dijalankan jika inputan selain angka 
     print("Masukkan yang Anda berikan tidak valid, harap masukkan angka!")
